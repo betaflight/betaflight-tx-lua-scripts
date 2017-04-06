7662066d7f3cba3427d08b7324ce9c189259dcd3
bbc25185 <bbc25185@libero.it>
2017-04-07 00:42:00 +0200

    Update Horus.lua

--- Horus.lua
+++ Horus.lua
@@ -70,7 +70,7 @@ backgroundFill = TEXT_BGCOLOR
 foregroundColor = LINE_COLOR
 globalTextOptions = TEXT_COLOR
 
-local run_ui = assert(loadScript("/SCRIPTS/BF/ui.lua"))()
+local run_ui = assert(loadScript("/SCRIPTS/common/ui.lua"))()
 
 drawScreenTitle = function (title)
   lcd.drawFilledRectangle(0, 0, LCD_W, 30, TITLE_BGCOLOR)
