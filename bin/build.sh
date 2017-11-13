1c217d27f498a47bf1b5eb3aec0b8edad7f830f8
Michael Keller <github@ike.ch>
2017-11-14 12:09:39 +1300

    Changed travis build to compile lua files.

--- bin/build.sh
+++ bin/build.sh
@@ -17,6 +17,7 @@ for f in ${MANIFEST[@]};
 do
     SRC_NAME=$f
     OBJ_NAME=$(dirname ${f})/$(basename ${f} .lua).luac
-    node -e "var luac = require('luac'); luac.buildToFileSync('${SRC_NAME}', '${OBJ_NAME}');"
+    luac -o ${OBJ_NAME} ${SRC_NAME} || exit 1
 done
 
+echo -e "\e[1m\e[39m[\e[31mTEST SUCCESSFUL\e[39m]\e[21m All lua files built successfully!"
