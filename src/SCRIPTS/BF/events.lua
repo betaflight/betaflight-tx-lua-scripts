018b271365d98c071e5c0b4821344ecb4c2d2cd4
Raphael Coeffic <rco@iptel.org>
2018-05-26 20:22:05 +0200

    new key mapping
    - RIGHT: next page
    - LEFT: previous page
    - UP: previous field / -
    - DOWN: next field / +
    - long RIGHT: menu

--- src/SCRIPTS/BF/events.lua
+++ src/SCRIPTS/BF/events.lua
@@ -5,11 +5,11 @@ return
         minus = EVT_MINUS_FIRST,
         plus = EVT_PLUS_FIRST,
         pageDown = EVT_PAGEDN_FIRST,
-        pageUp = EVT_PAGEUP_FIRST or EVT_UP_BREAK
+        pageUp = EVT_PAGEUP_FIRST or EVT_LEFT_BREAK
     },
     longPress = {
         enter = EVT_ENTER_LONG,
-        menu = EVT_MENU_LONG or EVT_DOWN_LONG
+        menu = EVT_MENU_LONG or EVT_RIGHT_LONG
     },
     repeatPress = {
         minus = EVT_MINUS_REPT,
@@ -18,12 +18,12 @@ return
     release = {
         enter = EVT_ENTER_BREAK,
         exit = EVT_EXIT_BREAK,
-        menu = EVT_MENU_BREAK or EVT_DOWN_BREAK,
+        menu = EVT_MENU_BREAK or EVT_RIGHT_BREAK,
         minus = EVT_MINUS_BREAK,
         plus = EVT_PLUS_BREAK
     },
     dial = {
-        left = EVT_ROT_LEFT or EVT_LEFT_BREAK,
-        right = EVT_ROT_RIGHT or EVT_RIGHT_BREAK
+        left = EVT_ROT_LEFT or EVT_UP_BREAK,
+        right = EVT_ROT_RIGHT or EVT_DOWN_BREAK
     }
 }
