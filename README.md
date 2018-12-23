1c544d5777865e9370866bac2fa827cb04025436
teckel12 <tim@leethost.com>
2018-12-23 14:04:01 -0500

    Added Horus support and standardized tab indenting, like adults do

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
-# Taranis VTx Lua Script
+# VTx Lua Script
 
-This stripped-down fork of [betaflight-tx-lua-scripts](https://github.com/betaflight/betaflight-tx-lua-scripts) includes only VTx configuration for Taranis transmitters only.  Taranis VTx was created to save memory on your transmitter so other Lua scripts (for example [Lua Telemetry](https://github.com/iNavFlight/LuaTelemetry)) can run on the same model. This script should work with [Betaflight](https://github.com/betaflight/betaflight) as well as [INAV](https://github.com/iNavFlight/inav).
+This stripped-down fork of [betaflight-tx-lua-scripts](https://github.com/betaflight/betaflight-tx-lua-scripts) includes only VTx configuration on FrSky transmitters.  VTx lua script was created to save memory on your transmitter so other Lua scripts (for example [Lua Telemetry](https://github.com/iNavFlight/LuaTelemetry)) can run on the same model. This script should work with [Betaflight](https://github.com/betaflight/betaflight) as well as [INAV](https://github.com/iNavFlight/inav).
 
 ### Features
 
 * Uses less memory than [betaflight-tx-lua-scripts](https://github.com/betaflight/betaflight-tx-lua-scripts)
 * Allows for multiple Lua scripts to be run on the same model (for example [Lua Telemetry](https://github.com/iNavFlight/LuaTelemetry))
 * Only allows for VTx control (no PID adjustments)
-* Works only on Taranis transmitters running SmartPort or F.Port telemetry
+* Works on Taranis and Horus transmitters running SmartPort or F.Port telemetry
 * Works with [INAV](https://github.com/iNavFlight/inav) and probably [Betaflight](https://github.com/betaflight/betaflight) (untested)
 
 ### Installation instructions
--- SCRIPTS/TELEMETRY/VTx/background.lua
+++ SCRIPTS/TELEMETRY/VTx/background.lua
@@ -8,86 +8,86 @@ local timeIsSet = false
 local mspMsgQueued = false
 
 local function getSensorValue()
-    if sensorId == -1 then
-        local sensor = getFieldInfo(protocol.stateSensor)
-        if type(sensor) == "table" then
-            sensorId = sensor['id'] or -1
-        end
-    end
-    return getValue(sensorId)
+	if sensorId == -1 then
+		local sensor = getFieldInfo(protocol.stateSensor)
+		if type(sensor) == "table" then
+			sensorId = sensor['id'] or -1
+		end
+	end
+	return getValue(sensorId)
 end
 
 local function modelActive(sensorValue)
-    return type(sensorValue) == "number" and sensorValue > 0
+	return type(sensorValue) == "number" and sensorValue > 0
 end
 
 local function init()
-    lastRunTS = 0
+	lastRunTS = 0
 end
 
 local function run_bg()
-    -- run in intervals
+	-- run in intervals
 
-    if lastRunTS == 0 or lastRunTS + INTERVAL < getTime() then
-        mspMsgQueued = false
-        -- ------------------------------------
-        -- SYNC DATE AND TIME
-        -- ------------------------------------
-        local sensorValue = getSensorValue()
+	if lastRunTS == 0 or lastRunTS + INTERVAL < getTime() then
+		mspMsgQueued = false
+		-- ------------------------------------
+		-- SYNC DATE AND TIME
+		-- ------------------------------------
+		local sensorValue = getSensorValue()
 
-        if not timeIsSet and modelActive(sensorValue) then
-            -- Send datetime when the telemetry connection is available
-            -- assuming when sensor value higher than 0 there is an telemetry connection
-            -- only send datetime one time after telemetry connection became available
-            -- or when connection is restored after e.g. lipo refresh
-            local now = getDateTime()
-            local year = now.year;
+		if not timeIsSet and modelActive(sensorValue) then
+			-- Send datetime when the telemetry connection is available
+			-- assuming when sensor value higher than 0 there is an telemetry connection
+			-- only send datetime one time after telemetry connection became available
+			-- or when connection is restored after e.g. lipo refresh
+			local now = getDateTime()
+			local year = now.year;
 
-            values = {}
-            values[1] = bit32.band(year, 0xFF)
-            year = bit32.rshift(year, 8)
-            values[2] = bit32.band(year, 0xFF)
-            values[3] = now.mon
-            values[4] = now.day
-            values[5] = now.hour
-            values[6] = now.min
-            values[7] = now.sec
+			values = {}
+			values[1] = bit32.band(year, 0xFF)
+			year = bit32.rshift(year, 8)
+			values[2] = bit32.band(year, 0xFF)
+			values[3] = now.mon
+			values[4] = now.day
+			values[5] = now.hour
+			values[6] = now.min
+			values[7] = now.sec
 
-            -- send msp message
-            protocol.mspWrite(MSP_SET_RTC, values)
-            mspMsgQueued = true
+			-- send msp message
+			protocol.mspWrite(MSP_SET_RTC, values)
+			mspMsgQueued = true
 
-            timeIsSet = true
-        elseif not modelActive(sensorValue) then
-            timeIsSet = false
-        end
+			timeIsSet = true
+		elseif not modelActive(sensorValue) then
+			timeIsSet = false
+		end
 
 
-        -- ------------------------------------
-        -- SEND RSSI VALUE
-        -- ------------------------------------
+		-- ------------------------------------
+		-- SEND RSSI VALUE
+		-- ------------------------------------
 
-        if mspMsgQueued == false then
-            local rssi, alarm_low, alarm_crit = getRSSI()
-            -- Scale the [0, 85] (empirical) RSSI values to [0, 255]
-            rssi = rssi * 3
-            if rssi > 255 then
-                rssi = 255
-            end
+		if mspMsgQueued == false then
+			local rssi, alarm_low, alarm_crit = getRSSI()
+			-- Scale the [0, 85] (empirical) RSSI values to [0, 255]
+			rssi = rssi * 3
+			if rssi > 255 then
+				rssi = 255
+			end
 
-            values = {}
-            values[1] = rssi
+			values = {}
+			values[1] = rssi
 
-            -- send msp message
-            protocol.mspWrite(MSP_TX_INFO, values)
-            mspMsgQueued = true
-        end
+			-- send msp message
+			protocol.mspWrite(MSP_TX_INFO, values)
+			mspMsgQueued = true
+		end
 
-        lastRunTS = getTime()
-    end
+		lastRunTS = getTime()
+	end
 
-    -- process queue
-    mspProcessTxQ()
+	-- process queue
+	mspProcessTxQ()
 
 end
 
