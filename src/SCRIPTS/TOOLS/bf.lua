d620ca4fedc34b96b75766961d0132b57192c1c7
Richard Cooper <richard.cooper@bennetts.co.uk>
2019-10-09 22:21:24 +0100

    Add to tools screen in OpenTx 2.3

---
+++ src/SCRIPTS/TOOLS/bf.lua
@@ -0,0 +1,15 @@
+local toolName = "TNS|Betaflight setup|TNE"
+SCRIPT_HOME = "/SCRIPTS/BF"
+
+apiVersion = 0
+
+protocol = assert(loadScript(SCRIPT_HOME.."/protocols.lua"))()
+radio = assert(loadScript(SCRIPT_HOME.."/radios.lua"))()
+
+assert(loadScript(radio.preLoad))()
+assert(loadScript(protocol.transport))()
+assert(loadScript(SCRIPT_HOME.."/MSP/common.lua"))()
+
+local run_ui = assert(loadScript(SCRIPT_HOME.."/ui.lua"))()
+
+return { run=run_ui }
