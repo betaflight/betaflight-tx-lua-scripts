37d091a13af7321bf6d616735f4beed52f6985dc
Hans Christian Olaussen <41271048+klutvott123@users.noreply.github.com>
2020-09-22 12:03:36 +0200

    128x96 support
    Adds support for radios using a 128x96 resolution display.

---
+++ src/SCRIPTS/BF/TEMPLATES/128x96/default_template.lua
@@ -0,0 +1,7 @@
+return {
+    margin       = 2,
+    indent       = 6,
+    lineSpacing  = 8,
+    listSpacing  = { line = 8, field = 88 },
+    tableSpacing = { row = 10, col = 30, header = 8 },
+}
