1535b18af42562c0bc534fa6661c665a540deff7
druckgott <forum@coolpixx.de>
2022-11-12 22:22:25 +0100

    Update name: acc_trim.lua

--- src/SCRIPTS/BF/pages.lua
+++ src/SCRIPTS/BF/pages.lua
@@ -57,7 +57,7 @@ if apiVersion >= 1.41 then
 end
 
 if apiVersion >= 1.16 then
-    PageFiles[#PageFiles + 1] = { title = "Trim Accelerometer", script = "acc_calibrate.lua" }
+    PageFiles[#PageFiles + 1] = { title = "Trim Accelerometer", script = "acc_trim.lua" }
 end
 
 return PageFiles
