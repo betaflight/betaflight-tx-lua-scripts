d25763d4759c4be3c6becc0cb7e05fb2b2f80d4b
druckgott <forum@coolpixx.de>
2022-11-13 16:38:14 +0100

    remove postload due to generic way

--- src/SCRIPTS/BF/PAGES/acc_trim.lua
+++ src/SCRIPTS/BF/PAGES/acc_trim.lua
@@ -24,14 +24,4 @@ return {
     minBytes    = 4,
     labels      = labels,
     fields      = fields,
-    postLoad = function(self)
-        temp_pitch=bit32.lshift(self.values[1], (1-1)*8)+bit32.lshift(self.values[2], (2-1)*8)
-        if temp_pitch >= 65000 then
-            self.fields[1].value = math.floor(temp_pitch-65536)
-        end
-        temp_roll=bit32.lshift(self.values[3], (1-1)*8)+bit32.lshift(self.values[4], (2-1)*8)
-        if temp_roll >= 65000 then
-            self.fields[2].value = math.floor(temp_roll-65536)
-        end
-    end,
 }
